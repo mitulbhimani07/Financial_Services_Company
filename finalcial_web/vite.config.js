4338c73982b8f99f3a3887911543f685aa0543fe
mitulbhimani07 <mitulbhimani281@gmail.com>
2025-04-16 12:32:52 +0530

    vite config

--- finalcial_web/vite.config.js
+++ finalcial_web/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +7,8 @@ export default defineConfig({
     port: 3000,
     host: true,
     },
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000 // Increase the limit to 1000 KB (adjust as needed)
+  }
 })
