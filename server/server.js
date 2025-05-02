acda5cce1d01cb201ac3e7bdfb5079582ba49f33
mitulbhimani07 <mitulbhimani281@gmail.com>
2025-05-02 12:27:43 +0530

    change port and server side

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var cors = require('cors')
 const app = express()
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 const db = require('./config/mongoDB')
 const dotenv = require('dotenv')
 require('dotenv').config()
